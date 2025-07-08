855cdac1a5a56e173df8b0963c60bfd131ee1742
Syed Mohamed S <syedking0075@gmail.com>
2025-07-08 16:22:02 +0530

    last commit

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { AppContextProvider  } from './Context/Appcontext.jsx'
+import { AppContextProvider  } from './Context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
     <BrowserRouter>
