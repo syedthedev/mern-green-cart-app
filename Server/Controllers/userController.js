e9e779952960f1c834a8d6f43011b2d673e8ee81
Syed Mohamed S <syedking0075@gmail.com>
2025-07-09 16:28:13 +0530

    One Last Commit

--- Server/Controllers/userController.js
+++ Server/Controllers/userController.js
@@ -21,7 +21,7 @@ export const register = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? 'None' : 'Lax',
+            sameSite: process.env.NODE_ENV === "production" ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -49,7 +49,7 @@ export const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? 'None' : 'Lax',
+            sameSite: process.env.NODE_ENV === "production" ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -65,7 +65,7 @@ export const logout = async (req, res) => {
         res.clearCookie('token', {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? 'None' : 'Lax'
+            sameSite: process.env.NODE_ENV === "production" ? 'none' : 'strict'
         });
         res.json({ success: true, msg: 'Logged Out' });
     } catch (err) {
